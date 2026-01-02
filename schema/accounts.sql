639c36b3c4981cff10d3ef270c0b32c6cdebd9bc
Samson <nazaritee@pm.me>
2026-01-02 11:39:11 -0600

    ui: more mobile responsivness

--- schema/accounts.sql
+++ schema/accounts.sql
@@ -7,5 +7,15 @@ CREATE TABLE
         source TEXT NOT NULL CHECK (source IN ('PLAID', 'APPLE')),
         account_type TEXT NOT NULL CHECK (account_type IN ('CREDIT', 'LOAN','INVESTMENT', 'DEPOSITORY')),
         balance INTEGER NOT NULL, -- stored in cents (e.g. $12.34 = 1234)
+        last_updated_at TEXT NOT NULL DEFAULT (datetime ('now')),
         FOREIGN KEY (plaid_id) REFERENCES plaid_accounts(id) ON DELETE CASCADE
     );
+    
+CREATE TRIGGER IF NOT EXISTS trg_accounts_last_updated
+AFTER UPDATE ON accounts
+FOR EACH ROW
+BEGIN
+    UPDATE accounts
+    SET last_updated_at = datetime('now')
+    WHERE id = OLD.id;
+END;
